20f100c1c708c2638c3d955e4fab4b62ba49d38e
Poldrack Lab <poldracklab@Poldracks-iMac.local>
2024-07-24 18:08:20 -0700

    Organizing files to match current version

--- simpleStop/simpleStop_lastrun.py
+++ simpleStop/simpleStop_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2022.2.2),
-    on Fri Jul 12 13:41:49 2024
+    on Wed Jul 24 16:50:40 2024
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -85,7 +85,7 @@ filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expNa
 # An ExperimentHandler isn't essential but helps with data saving
 thisExp = data.ExperimentHandler(name=expName, version='',
     extraInfo=expInfo, runtimeInfo=None,
-    originPath='/Users/poldracklab/Documents/jahrios/SharedControl/Code/simpleStop/simpleStop_lastrun.py',
+    originPath='/Users/poldracklab/Documents/jahrios/SharedControl/Code/SharedControl/simpleStop/simpleStop_lastrun.py',
     savePickle=True, saveWideText=True,
     dataFileName=filename)
 # save a log file for detail verbose info
