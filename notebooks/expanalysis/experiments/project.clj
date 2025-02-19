335800ceb928541191474c37250dc53e9413d3bd
kriti-a567 <kriti.a.64@gmail.com>
2025-02-18 17:00:49 -0800

    removed old notebooks

--- notebooks/expanalysis/experiments/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject art_1 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [quil "3.1.0"]])
-		:main "art_1.core"
