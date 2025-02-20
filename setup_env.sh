721ef44231449c6d48a2a81d4b1242a6a396c821
kriti-a567 <kriti.a.64@gmail.com>
2025-02-20 15:53:00 -0800

    changed setup_env

--- setup_env.sh
+++ setup_env.sh
@@ -1,11 +1,11 @@
 pip install uv
 
-uv venv
-
 source .venv/bin/activate
 
 uv sync
 
-uv pip install --editable .       
-cd notebooks  
-jupyter execute *.ipynb
+uv pip install --editable .
+uv pip install jupyter ipykernel
+uv add --dev ipykernel
+uv run ipython kernel install --user --env VIRTUAL_ENV $(pwd)/.venv --name=sharedcontrolpaper
+uv run --with jupyter jupyter lab
