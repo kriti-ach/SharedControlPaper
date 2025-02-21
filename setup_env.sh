0c8c21c1fb36bd37e119cef00ca995b0a3518849
Logan Bennett <bennett.j.logan@gmail.com>
2025-02-21 15:19:36 -0800

    trim depends, simplify setup

--- setup_env.sh
+++ setup_env.sh
@@ -1,12 +1,7 @@
 pip install uv
-uv venv
-
-source .venv/bin/activate
 
 uv sync
+source .venv/bin/activate
 
-uv pip install --editable .
-uv pip install jupyter ipykernel
-uv add --dev ipykernel
 uv run ipython kernel install --user --env VIRTUAL_ENV $(pwd)/.venv --name=sharedcontrolpaper
 uv run --with jupyter jupyter lab
